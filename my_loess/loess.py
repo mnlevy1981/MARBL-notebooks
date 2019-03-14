9b8e9b843d7983a8aa710eef0aeae857ae53d5b2
Michael Levy <mike.levy.work@gmail.com>
2019-03-14 16:25:47 -0600

    use np.matrix instead of np.array
    Slows some computation, but necessary for generalizing from 1D to n-D

--- my_loess/loess.py
+++ my_loess/loess.py
@@ -4,53 +4,56 @@
 import numpy as np
 
 class loess(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        assert x.size == y.size, "x and y must be same size"
-        self.npts = x.size
+    def __init__(self, x, data):
+        """ * x is a matrix of coordinates (nrow = number of dimensions, ncol = number of points)
+              -- vector x is converted to 1 x n matrix
+            * data is data sampled at x (to be smoothed)
+        """
+        self.x = np.asmatrix(x)
+        self.data = np.asmatrix(data)
+        assert x.shape[-1] == data.shape[-1], "x and fx must be same length"
+        self.npts = data.shape[-1]
 
     def poly_fit_at_i(self, i, n, d, t=0):
         """ Given an index i:
             1. Find the n x-coordinates closest to self.x[i]
             2. Fit a polynomial of degree d to those points
-            3. Return polynomial coefficients as well as all (self.x,self.y) values used to determine polynomial
+            3. Return polynomial coefficients as well as all (self.x, self.data) values used to determine polynomial
             4. Apply robustness t times
         """
         # Find n smallest values
         # https://stackoverflow.com/questions/5807047/efficient-way-to-take-the-minimum-maximum-n-values-and-indices-from-a-matrix-usi
-        hi = np.partition(np.abs(self.x[i] - self.x), n-1)[:n]
+        temp_array = np.linalg.norm(self.x[:,i] - self.x, axis=0)
+        hi = np.partition(temp_array, n-1)[:n]
         h = np.max(hi)
-        j_ind = np.argpartition(np.abs(self.x[i] - self.x), n-1)[:n]
-        xj = self.x[j_ind]
-        yj = self.y[j_ind]
+        j_ind = np.argpartition(temp_array, n-1)[:n]
+        xj = self.x[:,j_ind]
+        dataj = self.data[0,j_ind]
 
         # Regression: yj ~= sum b_n * (xj**n)
-        betas = np.polyfit(xj, yj, d, w=np.sqrt(_W(self.x[i] - xj, h)))[::-1]
+        assert self.x.shape[0] == 1, "Currently expecting 1d coordinates"
+        poly_data = np.poly1d(np.polyfit(np.squeeze(np.asarray(xj)), np.squeeze(np.asarray(dataj)), d, w=np.sqrt(_W(self.x[:,i] - xj, h))))
         for _ in range(t):
-            yhat = np.zeros(n)
-            for p, beta in enumerate(betas):
-                yhat += beta*(xj**p)
-            residuals = np.abs(yhat - yj)
+            data_est = poly_data(xj)
+            residuals = np.squeeze(np.asarray(np.abs(data_est - dataj)))
             s = np.median(residuals)
-            betas = np.polyfit(xj, yj, d, w=np.sqrt(_B(residuals/(6*s))*_W(self.x[i] - xj, h)))[::-1]
+            poly_data = np.poly1d(np.polyfit(np.squeeze(np.asarray(xj)), np.squeeze(np.asarray(dataj)), d, w=np.sqrt(_B(residuals/(6*s))*_W(self.x[:,i] - xj, h))))
 
-        return betas, xj, yj
+        return poly_data, xj, dataj
 
     def poly_fit(self, n, d, t=0):
-        yhat = np.zeros(self.npts)
+        data_est = np.zeros(self.npts)
         for i in range(0, self.npts):
-            betas, _, _ = self.poly_fit_at_i(i, n, d, t)
-            for p, beta in enumerate(betas):
-                yhat[i] += beta * (self.x[i]**p)
-        return yhat
+            poly_data, _, _ = self.poly_fit_at_i(i, n, d, t)
+            data_est[i] = poly_data(self.x[:,i])
+        return data_est
 
 def _W(x, h):
     """
     Tricube weight function from Cleveland (1979)
     We should only be calling this for points where |x| <= h
     """
-    return np.where(np.abs(x) < h, (1 - np.abs(x/h)**3)**3, 0)
+    return np.where(np.linalg.norm(x, axis=0) < h, (1 - np.linalg.norm(x/h, axis=0)**3)**3, 0)
 
 def _B(x):
-    return np.where(np.abs(x)<1, (1 - x**2)**2, 0)
+    return np.where(x < 1, (1 - x**2)**2, 0)
